fb13ce9a5960460646fa7a498368e195ae448ec6
Luis Felipe <luisfelipe@MACBOOK-PRO-DE-ADESKTOP.local>
2022-09-15 15:47:31 -0300

    criação do projeto

---
+++ src/TesteFuncionario.java
@@ -0,0 +1,12 @@
+public class TesteFuncionario {
+    public static void main(String[] args) {
+
+        Funcionario pipe = new Funcionario();
+        pipe.setNome("Luis Felipe");
+        pipe.setCpf("i453oi23423");
+        pipe.setSalario(4700.50);
+
+        System.out.println(pipe.getNome());
+        System.out.println(pipe.getBonificacao());
+    }
+}

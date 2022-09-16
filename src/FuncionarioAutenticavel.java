393dddeb0caf9684356543abb0b7532fe3bf26e3
Luis Felipe <luisfelipe@MACBOOK-PRO-DE-ADESKTOP.local>
2022-09-16 12:50:16 -0300

    implementacao da classe FuncionarioAutenticavel

---
+++ src/FuncionarioAutenticavel.java
@@ -0,0 +1,12 @@
+public abstract class FuncionarioAutenticavel extends Funcionario {
+
+    private int senha;
+
+    public boolean autentica(int senha) {
+        return this.senha == senha;
+    }
+
+    public void setSenha(int senha) {
+        this.senha = senha;
+    }
+}
